1145c00a209ae9e4d2c8d68d0459703dbeefe358
vijaybajaj12 <58513404+vijaybajaj12@users.noreply.github.com>
2019-12-06 15:27:34 +0530

    Initial changes with UI integration

---
+++ src/main/java/com/containercrush/ContcrushApplication.java
@@ -0,0 +1,13 @@
+package com.containercrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContcrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContcrushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/container/contcrush/ContcrushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.container.contcrush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContcrushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
