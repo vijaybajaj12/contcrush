23ca7aedcfa8080c38967432241d7db895296b9a
sanjibpal10 <58432293+sanjibpal10@users.noreply.github.com>
2019-12-20 16:31:54 +0530

    Changes with basic search functionalities

--- src/main/resources/static/script/koolApp.js
+++ src/main/resources/static/script/koolApp.js
@@ -2,23 +2,21 @@ angular.module('koolApp', [])
 .controller('koolCtrl', function($scope, $http) {
 $scope.test=null;
 $scope.ClassId1=null;
-     $http.get('http://localhost:8080/koolApp/findSegments/').
+     $http.get('/koolApp/findSegments/').
         then(function(response) {
             $scope.Segments= response.data;
  			$scope.test=$scope.Segments[0]; 
          });
         
  $scope.OnSegmentChange = function(ID) {
-     $http.get("http://localhost:8080/koolApp/findFamiliesBySegment/"+$scope.Segments).
+     $http.get("/koolApp/findFamiliesBySegment/"+$scope.Segments).
             then(function (responses) {  
                 $scope.Families = responses.data;   
           });   
          }  
  
  $scope.OnFamilyChange = function() {
-  //var familyid=$scope.Families[1].familyId;
-  //alert($scope.Family);
-       $http.get("http://localhost:8080/koolApp/findClassByFamily/"+$scope.Family).
+        $http.get("/koolApp/findClassByFamily/"+$scope.Family).
             then(function (responses) {  
                 $scope.Classes = responses.data;
 				$scope.ClassId1=$scope.Classes[0].classId;
@@ -27,12 +25,11 @@ $scope.ClassId1=null;
          }  
  
  $scope.OnClassChange = function() {
-   alert($scope.Classes);   
-  //alert('hello'+$scope.Classes[0].classId);
-      $http.get("http://localhost:8080/koolApp/findCommoditiesByClass/"+$scope.Classes).
+  
+   $http.get("/koolApp/findCommoditiesByClass/"+$scope.Classes).
             then(function (responses) {  
-                $scope.comodity = responses.data;
-                alert($scope.comodity[0].styleItemNumber);   
+               $scope.comodity = responses.data;
+                //alert($scope.comodity[0].styleItemNumber);   
           });   
          }  
       
