ebf016b4023d23c5af55fd25c7c529d92915364f
VIJAY BAJAJ <vijay.bajaj@in.ibm.com>
2020-01-03 16:20:29 +0530

    constant file

---
+++ src/main/java/com/ibm/nscontainercrush/constant/ContainerCrushConstant.java
@@ -0,0 +1,12 @@
+package com.ibm.nscontainercrush.constant;
+
+public class ContainerCrushConstant {
+	
+	public static final String SINGLE_QUOTE = "'";
+	public static final String PERCENT = "%";
+	public static final String OR_OPERATOR = " OR ";
+	public static final String AND_OPERATOR = " AND ";
+	public static final String LEFT_BRACE = "(";
+	public static final String RIGHT_BRACE = ")";
+
+}
