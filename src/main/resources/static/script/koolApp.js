0897b08b238e95304f98545486cdecaaf430595d
sanjibpal10 <58432293+sanjibpal10@users.noreply.github.com>
2020-01-08 20:02:53 +0530

    final change

--- src/main/resources/static/script/koolApp.js
+++ src/main/resources/static/script/koolApp.js
@@ -2,35 +2,48 @@ angular.module('koolApp', [])
 .controller('koolCtrl', function($scope, $http) {
 $scope.test=null;
 $scope.ClassId1=null;
-     $http.get('/koolApp/findSegments/').
-        then(function(response) {
-            $scope.Segments= response.data;
- 			$scope.test=$scope.Segments[0]; 
-         });
-        
- $scope.OnSegmentChange = function(ID) {
-     $http.get("/koolApp/findFamiliesBySegment/"+$scope.Segments).
+$scope.show=0;
+//$scope.selectedSegment='';
+ //    $http.get('/koolApp/findSegments/').
+  //      then(function(response) {
+   //         $scope.Segments= response.data;
+ //			$scope.test=$scope.Segments[0]; 
+  //       });
+
+$http.get("/koolApp/findFamiliesBySegment/53000000").
             then(function (responses) {  
-                $scope.Families = responses.data;   
-          });   
-         }  
+                $scope.Families = responses.data;
+                   
+          });
  
- $scope.OnFamilyChange = function() {
-        $http.get("/koolApp/findClassByFamily/"+$scope.Family).
+// $scope.OnSegmentChange = function () {
+//alert($scope.Segments[0].segmentId);
+//$http.get("/koolApp/findFamiliesBySegment/53000000").
+  //          then(function (responses) {  
+   //             $scope.Families = responses.data;   
+    //      });
+//}
+
+ $scope.OnFamilyChange = function(Families) {
+// alert("family Id"+$scope.SelectedFamilyId);
+         $http.get("/koolApp/findClassesByFamily/"+$scope.SelectedFamilyId).
             then(function (responses) {  
                 $scope.Classes = responses.data;
 				$scope.ClassId1=$scope.Classes[0].classId;
-				
+				$scope.show=1;
           });   
          }  
- 
+  
  $scope.OnClassChange = function() {
-  //alert($scope.Classes);
-   $http.get("/koolApp/findCommoditiesByClass/"+$scope.Classes).
+  alert($scope.SelectedClassId);
+   $http.get("/koolApp/findCommoditiesByClass/"+$scope.SelectedClassId).
             then(function (responses) {  
                $scope.comodity = responses.data;
+           //    alert(responses.data);
                 //alert($scope.comodity[0].styleItemNumber);   
           });   
          }  
+      
+
       
 });
