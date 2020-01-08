494e14eb6539b33e5947d58beca773adfaaf3e55
VIJAY BAJAJ <vijay.bajaj@in.ibm.com>
2020-01-08 15:38:06 +0530

    changed http type from POST to GET for convertSpeechToText

--- src/main/java/com/ibm/nscontainercrush/controller/SpeechToTextController.java
+++ src/main/java/com/ibm/nscontainercrush/controller/SpeechToTextController.java
@@ -3,7 +3,7 @@ package com.ibm.nscontainercrush.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class SpeechToTextController {
 	@Autowired
 	private SpeechToTextService sttService;
 	
-	@PostMapping("/convertSpeechToText")
+	@GetMapping("/convertSpeechToText")
 	public List<String> convertSpeechToText(){
 		return sttService.convertSpeechToText();
 	}
