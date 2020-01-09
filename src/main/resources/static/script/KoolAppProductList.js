777b17e9da7c7c3a4c0ae92c38f2cb2d23554d79
sanjibpal10 <58432293+sanjibpal10@users.noreply.github.com>
2020-01-09 16:27:29 +0530

    Layout changes

---
+++ src/main/resources/static/script/KoolAppProductList.js
@@ -0,0 +1,21 @@
+angular.module('koolAppProductApp', [])
+.controller('ProductListController', function($scope, $http, $window) {
+ $scope.commodityId = $window.location.search.substring(4);
+ $scope.show=0;
+ $scope.hide=0;
+      $http.get('/koolApp/findItemsByCommodity/'+$scope.commodityId).
+        then(function(response) {
+            $scope.comodity= response.data;
+            $scope.show=1;
+            if(response.data=="")
+            {
+                 	$scope.hide=1;
+            }
+            
+ 			//alert('id'+ response.data);
+         });
+         
+             
+ 
+    
+});
