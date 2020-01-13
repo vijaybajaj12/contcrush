9c492c139111e715f826c147f0e65d1511e0aebe
VIJAY BAJAJ <vijay.bajaj@in.ibm.com>
2020-01-13 15:55:13 +0530

    class to hold List<SkuItem>

---
+++ src/main/java/com/ibm/nscontainercrush/dto/SkuItemResult.java
@@ -0,0 +1,17 @@
+package com.ibm.nscontainercrush.dto;
+
+import java.util.List;
+
+public class SkuItemResult {
+	
+	private List<SkuItem> skuItemList;
+
+	public List<SkuItem> getSkuItemList() {
+		return skuItemList;
+	}
+
+	public void setSkuItemList(List<SkuItem> skuItemList) {
+		this.skuItemList = skuItemList;
+	}
+
+}
